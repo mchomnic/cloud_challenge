faaeead11c9c59b50f68dafc6abd3dfc3934aa22
Marek Chomnicki <marek.chomnicki@intel.com>
2024-10-14 18:38:15 +0200

    aws: update backend on new aws account
    Signed-off-by: Marek Chomnicki <marek.chomnicki@intel.com>

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "webapp-terraform-state"
-    key            = "terraform/state/dev.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
+    bucket  = "web-one-page-state"
+    key     = "terraform.tfstate"
+    encrypt = true
+    region  = "eu-central-1"
     # dynamodb_table = "WebApp-TerraformLocks-dev"  # Uncomment this line to enable DynamoDB table for locking
   }
 }
-
